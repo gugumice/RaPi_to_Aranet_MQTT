145129093f30293f84c511d032e1f7b58d14cf9f
Gunars Frickauss <gunarsf@gmail.com>
2024-01-30 12:27:09 +0200

    Update initpi.sh

--- initpi.sh
+++ initpi.sh
@@ -3,15 +3,9 @@ if [ ! -e /home/pi ]; then
     echo "Only run this on your pi."
     exit 1
 fi
-systemctl enable kiosk.service
+systemctl enable temperatures.service
 systemctl disable firstboot.service
 raspi-config --expand-rootfs > /dev/null
-CURRENT_HOSTNAME=$(cat /proc/sys/kernel/hostname)
-IPO=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1 |  cut -d. -f2);
-NEW_HOSTNAME="rpi-kio4-"$IPO
-echo $CURRENT_HOSTNAME
-echo $NEW_HOSTNAME
-sleep 1
-hostnamectl set-hostname ${NEW_HOSTNAME} --static
+python3 /opt/temps/config_temps.py
 echo "01 10 * * * sudo shutdown -r" >>  /var/spool/cron/crontabs/root
 /sbin/shutdown -r now
