45e521ed7d04a6953ff4de6e75ef2234933c8bc7
Gunars Frickauss <gunarsf@gmail.com>
2024-01-31 12:28:06 +0200

    Update change_hostname.sh

--- change_hostname.sh
+++ change_hostname.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # /usr/sbin/change_hostname.sh - program to permanently change hostname.  
-
-
-newHostname=grep -E '^Serial' /proc/cpuinfo | tail -n 1 | cut -c-16
-# $1 - new hostname, should be a legal hostname
-hostnamectl set-hostname ${$1} --static
+# $1 - new hostname prefix, should be a legal hostname
+newHostname=${$1}$(grep -E '^Serial' /proc/cpuinfo | tail -n 1 | cut -c 22-28)
+hostnamectl set-hostname $newHostname --static
