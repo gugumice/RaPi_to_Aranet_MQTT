2328e3a8913b994f0b70dc2eb07fa8200cc390c8
Gunars Frickauss <gunarsf@gmail.com>
2024-01-31 11:18:34 +0200

    Update change_hostname.sh

--- change_hostname.sh
+++ change_hostname.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-# /usr/sbin/change_hostname.sh - program to permanently change hostname.  Permissions
-# are set so that www-user can `sudo` this specific program.
+# /usr/sbin/change_hostname.sh - program to permanently change hostname.  
 
-# args:
+
+newHostname=grep -E '^Serial' /proc/cpuinfo | tail -n 1 | cut -c-16
 # $1 - new hostname, should be a legal hostname
 hostnamectl set-hostname ${$1} --static
-#sed -i "s/$HOSTNAME/$1/g" /etc/hosts
-#echo $1 > /etc/hostname
-#/etc/init.d/hostname.sh
-#subprocess.run(['sudo', '/usr/sbin/change_hostname.sh', newhostname])
