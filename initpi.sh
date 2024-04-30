9f9f008add844ca7335757f96341fb6f6f824f1d
Gunars Frickauss <gunarsf@gmail.com>
2024-04-30 15:55:26 +0300

    Update initpi.sh

--- initpi.sh
+++ initpi.sh
@@ -12,11 +12,9 @@ echo "01 10 * * * sudo shutdown -r" >>  /var/spool/cron/crontabs/root
 sleep 1
 newHostname=rapi-mqtt$(grep -E '^Serial' /proc/cpuinfo | tail -n 1 | cut -c 22-28)
 hostnamectl set-hostname $newHostname --static
-sed -i '/^#NTP=.*/a FallbackNTP=laiks.egl.local' /etc/systemd/timesyncd.conf
 echo ${newHostname} > /etc/hostname
 sed -i '/^127.0.0.1/s/.*/127.0.0.1\t'${newHostname}'/g' /etc/hosts
-#chattr -i /etc/hosts
+sed -i '/^#NTP=.*/a FallbackNTP=laiks.egl.local' /etc/systemd/timesyncd.conf
 echo '10.100.20.104   laiks.egl.local' >> /etc/hosts
 echo '10.100.50.102   cache.egl.local' >> /etc/hosts
-#chattr +i /etc/hosts
 /sbin/shutdown -r now
