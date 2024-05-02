4a976c3fb36120f08c4b75803e333f9257a95d18
Gunars Frickauss <gunarsf@gmail.com>
2024-05-02 15:42:23 +0300

    Update initpi.sh

--- initpi.sh
+++ initpi.sh
@@ -6,8 +6,6 @@ fi
 systemctl enable thermometers.service
 systemctl disable firstboot.service
 raspi-config --expand-rootfs > /dev/null
-sleep 3
-python3 /opt/temps/init_temps.py
 echo "01 10 * * * sudo shutdown -r" >>  /var/spool/cron/crontabs/root
 sleep 1
 newHostname=rapi-mqtt$(grep -E '^Serial' /proc/cpuinfo | tail -n 1 | cut -c 22-28)
@@ -17,4 +15,6 @@ sed -i '/^127.0.0.1/s/.*/127.0.0.1\t'${newHostname}'/g' /etc/hosts
 sed -i '/^#NTP=.*/a FallbackNTP=laiks.egl.local' /etc/systemd/timesyncd.conf
 echo '10.100.20.104   laiks.egl.local' >> /etc/hosts
 echo '10.100.50.102   cache.egl.local' >> /etc/hosts
+python3 /opt/thermo/init_temps.py
+sleep 3
 /sbin/shutdown -r now
